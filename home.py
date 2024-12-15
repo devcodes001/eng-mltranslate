4c3734ff5bb6e82c12b2ef991c1d9fa19396b20c
dev <devnarayanpunthala3@gmail.com>
2024-12-15 06:31:25 +0000

    'm'

--- home.py
+++ home.py
@@ -1,2 +1,20 @@
 import streamlit as st
-st.write('helllo')
+import requests
+from bs4 import BeautifulSoup
+from newspaper import Article
+from deep_translator import GoogleTranslator
+
+st.title("Translator")
+
+url = st.text_input("Enter the URL of the webpage you want to scrape:")
+
+if url:
+    article = Article(url)
+    article.download()
+    article.parse()
+    st.text_area("News : ",article.text)
+    translated_text = GoogleTranslator(source='en', target='ml').translate(article.text)
+    st.text_area("Malayalam",translated_text,height = 500)
+
+
+ 
